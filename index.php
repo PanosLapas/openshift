f7c13c82af937504990409204b6eb89ded75bebc
PanosLapas <panagoslap@gmail.com>
2017-06-23 17:07:54 +0300

    add favicon

--- index.php
+++ index.php
@@ -11,8 +11,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
 		<link rel="stylesheet" href="assets/css/main.css" />
-		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
-		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<link rel="icon" href="images/Picture1.png" type="image/gif" sizes="16x16">
 		<style>
 			#myBtn {
 			  display: none;
